799a017731b8977fcb885d392ecaf8d344066ea2
ELCesarMaat <cesarmaat@hotmail.com>
2023-04-29 20:47:40 -0600

    Update Persons.kt

--- app/src/main/java/com/robles/itcm/ptampersonas/Persons.kt
+++ app/src/main/java/com/robles/itcm/ptampersonas/Persons.kt
@@ -2,4 +2,10 @@ package com.robles.itcm.ptampersonas
 
 import android.graphics.Bitmap
 
-data class Persons(var title: String, var description: String, var image: Bitmap, var curp: String, var enabled: Boolean)
+data class Persons(
+    var title: String,
+    var description: String,
+    var image: Bitmap,
+    var curp: String,
+    var enabled: Boolean
+    )
